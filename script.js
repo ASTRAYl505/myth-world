8db4fb8d2fb46c9418ac60fecc720cb0126ccf24
ASTRAYl505 <nwjfbwbwwf@privaterelay.appleid.com>
2026-06-29 22:17:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-let hoverSound = new Audio("https://cdn.pixabay.com/audio/2022/03/15/audio_c8c8f1a8f5.mp3");
+function start(){
+document.querySelector(".intro").style.display="none";
+}
 
-/* ✨ دخول تدريجي واحد واحد */
+/* ✨ show cards one by one */
 window.addEventListener("load",()=>{
 
-let cards = document.querySelectorAll(".planet-card");
+let cards=document.querySelectorAll(".card");
 
-cards.forEach((card,i)=>{
+cards.forEach((c,i)=>{
 setTimeout(()=>{
-card.classList.add("show");
-}, i * 200); // واحد واحد
-});
-
-});
-
-/* 🔊 صوت hover */
-document.querySelectorAll(".planet-card").forEach(card=>{
-
-card.addEventListener("mouseenter",()=>{
-
-hoverSound.currentTime = 0;
-hoverSound.volume = 0.2;
-hoverSound.play();
-
+c.classList.add("show");
+},i*200);
 });
 
 });
